d3aebe865045802a121f3cd8787df52ca7e3c07e
Akshay-47 <akshay.lokray47@gmail.com>
2020-12-13 23:46:30 +0530

    add gh-pages module

--- src/utils/ModalController.js
+++ src/utils/ModalController.js
@@ -3,29 +3,29 @@ import EventEmitter from "../lib/EventEmitter";
 export const uiEvents = new EventEmitter();
 
 const modalController = () => {
-  let queue = [];
+  let stack = [];
   let currentLevel = 0;
 
   return {
     open(name) {
       // If modal already open bring it to front
-      if (queue.includes(name)) {
-        queue = queue.filter((o) => o !== name);
+      if (stack.includes(name)) {
+        stack = stack.filter((o) => o !== name);
       }
-      queue.push(name);
+      stack.push(name);
       currentLevel += 1;
       uiEvents.emit(`${name}.open`, { level: currentLevel });
       uiEvents.emit("overlay$open", { level: currentLevel });
     },
     close() {
-      if (queue.length > 0) {
-        const name = queue.pop();
+      if (stack.length > 0) {
+        const name = stack.pop();
         uiEvents.emit(`${name}.close`);
-        if (queue.length === 0) {
+        if (stack.length === 0) {
           uiEvents.emit("overlay$close");
           currentLevel = 0;
         } else {
-          const activeModalName = queue[queue.length - 1];
+          const activeModalName = stack[stack.length - 1];
           currentLevel -= 1;
           uiEvents.emit("overlay$level", { level: currentLevel });
           uiEvents.emit(`${activeModalName}.level`, {
